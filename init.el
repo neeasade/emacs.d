2afdbdefb562e7a9ccd6e0ac67480449cf7ea9a1
neeasade <nathanisom27@gmail.com>
2018-11-12 22:07:08 -0600

    typo

--- init.el
+++ init.el
@@ -7,112 +7,112 @@
 
 (defun ns/add-firstrun-action (action)
   (setq ns/firstrun-action
-		(cons action ns/firstrun-action)))
+    (cons action ns/firstrun-action)))
 
 (setq
- ns/enable-windows-p (eq system-type 'windows-nt)
- ns/enable-linux-p (eq system-type 'gnu/linux)
- ns/enable-home-p (string= (system-name) "erasmus")
- ns/enable-docker-p (string= (getenv "USER") "emacser")
- ns/enable-work-p ns/enable-windows-p
- ns/enable-colemak t
- ;; for when we're away from $HOME.
- ns/xrdb-fallback-values
- `(
-   ;; ("*.background"         . ,(face-attribute 'default :background))
-   ("*.background"         . nil)
-   ("Emacs.powerlinescale" . "1.1")
-   ("Emacs.theme"          . "base16-grayscale-light")
-   ("emacs.powerline"      . "bar")
-   ("st.borderpx"          . "0")
-   ("st.font"              . "Go Mono-10")
-   ("st.font_variable"     . "Go-10")
-   ))
+  ns/enable-windows-p (eq system-type 'windows-nt)
+  ns/enable-linux-p (eq system-type 'gnu/linux)
+  ns/enable-home-p (string= (system-name) "erasmus")
+  ns/enable-docker-p (string= (getenv "USER") "emacser")
+  ns/enable-work-p ns/enable-windows-p
+  ns/enable-colemak t
+  ;; for when we're away from $HOME.
+  ns/xrdb-fallback-values
+  `(
+     ;; ("*.background"         . ,(face-attribute 'default :background))
+     ("*.background"         . nil)
+     ("Emacs.powerlinescale" . "1.1")
+     ("Emacs.theme"          . "base16-grayscale-light")
+     ("emacs.powerline"      . "bar")
+     ("st.borderpx"          . "0")
+     ("st.font"              . "Go Mono-10")
+     ("st.font_variable"     . "Go-10")
+     ))
 
 (setq load-prefer-newer t)
 (eval-and-compile (load "~/.emacs.d/lisp/theworld.el"))
 
 (defmacro ns/load (&rest targets)
   `(mapc (lambda(target)
-		   (funcall (intern (concat "ns/" (prin1-to-string target)))))
-		 ',targets))
+           (funcall (intern (concat "ns/" (prin1-to-string target)))))
+     ',targets))
 
 (defmacro ns/compose (name &rest targets)
   `(defconfig ,name (ns/load ,@targets)))
 
 (ns/compose
- core
+  core
 
- ;; use-package
- straight
- bedrock
- sanity
- evil
- interface
- editing
- shell
- git
- org
- util
- server
- )
-
-(ns/compose
- extra
-
- company
- flycheck
- jump
- dashdocs
-
- terminal
- ;; zoom
- ;; dimmer
- projectile
- treemacs
- restclient
- latex
- search-engines
-
- target-process
- music
- pdf
- ledger
- emoji
- filehooks
- writing
- deadgrep
- elasticsearch
- graphiz
- )
+  ;; use-package
+  straight
+  bedrock
+  sanity
+  evil
+  interface
+  editing
+  shell
+  git
+  org
+  util
+  server
+  )
 
 (ns/compose
- development
+  extra
 
- autohotkey
- clojure
- csharp
- elisp
- nix
- javascript
- typescript
- markdown
- powershell
- lua
- guix
- ;; lsp
- ;; terraform
- ;; sql
- ;; jekyll
- ;; plantuml
- )
+  company
+  flycheck
+  jump
+  dashdocs
+
+  terminal
+  ;; zoom
+  ;; dimmer
+  projectile
+  treemacs
+  restclient
+  latex
+  search-engines
+
+  targetprocess
+  music
+  pdf
+  ledger
+  emoji
+  filehooks
+  writing
+  deadgrep
+  elasticsearch
+  graphiz
+  )
 
 (ns/compose
- communication
+  development
 
- irc slack twitter email
- reddit stackexchange elfeed
- )
+  autohotkey
+  clojure
+  csharp
+  elisp
+  nix
+  javascript
+  typescript
+  markdown
+  powershell
+  lua
+  guix
+  ;; lsp
+  ;; terraform
+  ;; sql
+  ;; jekyll
+  ;; plantuml
+  )
+
+(ns/compose
+  communication
+
+  irc slack twitter email
+  reddit stackexchange elfeed
+  )
 
 ;; liftoff
 (ns/load core extra development communication staging check-for-orphans)
