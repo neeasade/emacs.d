09bcb746d7b513173d4d19fcd1683aaed2e29104
neeasade <neeasade@gmail.com>
2019-05-07 21:47:09 -0400

    moved

--- lisp/trees/staging.el
+++ lisp/trees/staging.el
@@ -128,40 +128,6 @@ Version 2018-02-21"
       (color-name-to-rgb end)
       steps)))
 
-(defun shell-sync-dir-with-prompt (string)
-  "A preoutput filter function (see `comint-preoutput-filter-functions')
-which sets the shell buffer's path to the path embedded in a prompt string.
-This is a more reliable way of keeping the shell buffer's path in sync
-with the shell, without trying to pattern match against all
-potential directory-changing commands, ala `shell-dirtrack-mode'.
-
-In order to work, your shell must be configured to embed its current
-working directory into the prompt.  Here is an example .zshrc
-snippet which turns this behavior on when running as an inferior Emacs shell:
-
-  if [ $EMACS ]; then
-     prompt='|Pr0mPT|%~|[%n@%m]%~%# '
-  fi
-
-The part that Emacs cares about is the '|Pr0mPT|%~|'
-Everything past that can be tailored to your liking.
-"
-  (if (string-match "|Pr0mPT|\\([^|]*\\)|" string)
-    (let ((cwd (match-string 1 string)))
-      (setq default-directory
-        (if (string-equal "/" (substring cwd -1))
-          cwd
-          (setq cwd (concat cwd "/"))))
-      (replace-match "" t t string 0))
-    string))
-
-(defun ns/shell-track ()
-  (shell-dirtrack-mode nil)
-  (add-hook 'comint-preoutput-filter-functions 'shell-sync-dir-with-prompt nil t))
-
-(add-hook 'shell-mode-hook 'ns/shell-track)
-
-
 (defvar ns/gothic-table
   (let ((str (make-string 127 0)))
     (dotimes (i 127)
@@ -184,4 +150,4 @@ Everything past that can be tailored to your liking.
   (translate-region beg end ns/gothic-table))
 
 (defun ns/text-to-widechar (beg end) (interactive "r")
-  (translate-region beg end ns/gothic-table))
+  (translate-region beg end ns/widechar-table))
