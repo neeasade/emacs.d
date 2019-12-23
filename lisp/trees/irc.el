ad2ced5c5b887e1a7bc19412729b34c100bc5918
neeasade <neeasade@gmail.com>
2019-12-22 23:20:51 -0500

    blame cake^

--- lisp/trees/irc.el
+++ lisp/trees/irc.el
@@ -188,6 +188,7 @@
                  "cappuccino"
                  "EGGServ"
                  "EggServ"
+                 "CuteServ"
                  "linkreader"
                  ) nick)
           (s-ends-with-p "bot" nick))
