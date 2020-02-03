c94607bccf1393d872e482534db08ea7ba65e888
neeasade <neeasade@gmail.com>
2020-02-03 12:24:39 -0500

    checkpoint

--- lisp/trees/doomline.el
+++ lisp/trees/doomline.el
@@ -1,7 +1,19 @@
-;; trying out doom-modeline
 (use-package doom-modeline)
 
-;; doom stuff that I'm tweaking
+;; override this to make the middle line have a different face (non 'mode-line)
+(defface ns/mode-line-middle nil "middle mode line" :group 'doom-modeline-faces)
+
+(set-face-attribute 'ns/mode-line-middle nil :background
+  ;; (ns/color-greaten 20 (face-attribute 'mode-line :background))
+  ;; (ns/color-greaten 20 (face-attribute 'default :background)
+
+  (face-attribute 'default :background)
+  )
+
+(doom-modeline-def-modeline 'main
+  '(bar workspace-name window-number modals matches buffer-info remote-host buffer-position word-count parrot selection-info)
+  '(objed-state misc-info persp-name battery grip irc mu4e gnus github debug lsp minor-modes input-method indent-info buffer-encoding major-mode process vcs checker))
+
 ;; overridding because upstream is really spacey
 (doom-modeline-def-segment buffer-position
   "The buffer position information."
@@ -11,8 +23,7 @@
                    (doom-modeline-column-zero-based "%l:%c" "%l:%C")
                    "%l")
                  (column-number-mode (doom-modeline-column-zero-based ":%c" ":%C"))))
-          (face (if active 'ns/mode-line-segment
-                  'mode-line-inactive))
+          (face (if active 'mode-line 'mode-line-inactive))
           (mouse-face 'mode-line-highlight)
           (local-map mode-line-column-line-number-mode-map))
     (concat
@@ -25,25 +36,127 @@
       (doom-modeline-spc)
       )))
 
-;; we want segments to have different faces than the mid part, so we must invert the world.
-(defface ns/mode-line-segment nil "segment face" :group 'doom-modeline-faces)
+(letf (((symbol-function 'set-window-buffer-start-and-point)
+         (fn nil))
+        ((symbol-function 'set-window-prev-buffers)
+          (fn nil)))
+  (switch-to-prev-buffer))
 
-(set-face-attribute 'ns/mode-line-segment nil :background
-  (ns/color-greaten 12 (face-attribute 'mode-line :background)))
-;; (set-face-attribute 'ns/mode-line-segment-inactive nil :background (face-attribute 'mode-line-inactive :background))
+(letf (((symbol-function 'set-window-buffer-start-and-point)
+         (fn nil))
+        ((symbol-function 'set-window-next-buffers)
+          (fn nil))
+        ((symbol-function 'set-window-prev-buffers)
+          (fn nil))
+        )
+  (switch-to-next-buffer))
+
 
 (defsubst doom-modeline-spc ()
-  "Text style with whitespace"
-  (propertize " " 'face (if (doom-modeline--active)
-                          'ns/mode-line-segment
+  "Text style with whitespace."
+  (propertize " " 'face (if (doom-modeline--active)
+                          'mode-line
                           'mode-line-inactive)))
 
 (doom-modeline-def-segment buffer-info-neeasade
   (concat
     (doom-modeline-spc)
+    (doom-modeline-spc)
     (doom-modeline--buffer-name)
     ;; (doom-modeline--buffer-file-name)
     (doom-modeline--buffer-state-icon)
+    ))
+
+;; note to self: abandon this, look at switch-to-next, prev-buffer source, grab that and replace return
+(defun ns/next-buffer-name ()
+  (->> (window-next-buffers)
+    ;; (-map 'first)
+    (-map 'buffer-name)
+    ;; (-drop 1)
+    (-filter (fn (not (string= (buffer-name (current-buffer)) <>))))
+    (-first (fn (not (ns/should-skip <>))))
+    ;; (current-buffer)
+    )
+
+  )
+
+(defun ns/prev-buffer-name ()
+  (->>
+    (window-prev-buffers)
+    ;; (reverse)
+    (-map 'first)
+    (-map 'buffer-name)
+    ;; (-drop 1)
+    (-filter (fn (not (string= (buffer-name (current-buffer)) <>))))
+    (-filter (fn (not (string= (ns/next-buffer-name) <>))))
+    (-first (fn (not (ns/should-skip <>))))
+    ;; (current-buffer)
+    )
+
+  )
+
+(doom-modeline-def-segment next-buffers
+  (when (doom-modeline--active)
+    (propertize
+      (concat
+        ;; todo: mabye [ <buf:-> ¦ <buf:-> ]
+        (doom-modeline-spc)
+        "["
+        (or (s-left 8 (ns/prev-buffer-name)) "<None>")
+        "|"
+        (or (s-left 8 (ns/next-buffer-name)) "<None>")
+        "]"
+        (doom-modeline-spc)
+        ;; (doom-modeline-spc)
+        )
+      'face 'mode-line
+      )))
+
+;; using our own separators
+(defface ns/mode-line-sep nil "sep" :group 'doom-modeline-faces)
+(set-face-attribute 'ns/mode-line-sep nil :background
+  ;; (ns/color-greaten 10 (face-attribute 'mode-line :background))
+  ;; "#8888cc"
+  (face-attribute 'default :background)
+  ;; (face-attribute 'default)
+  )
+
+(set-face-attribute 'mode-line nil :background
+  (ns/color-lessen 20 (face-attribute 'default :background))
+  ;; (face-attribute 'default :background)
+  )
+
+(doom-modeline-def-segment sep
+  "Text style with whitespace."
+
+  (concat
+    ;; (doom-modeline-spc)
+    (propertize " " 'face (if (doom-modeline--active)
+                            'ns/mode-line-sep
+                            'mode-line-inactive))
+    ;; (doom-modeline-spc)
+    ))
+
+  (defface ns/mode-line-sep-edge nil "sep-edge" :group 'doom-modeline-faces)
+
+(set-face-attribute 'ns/mode-line-sep-edge nil :background
+
+  (face-attribute 'default :background)
+  ;; "#000000"
+  )
+
+;; we want to be darker on the edges
+;; ref: thin space: ''
+;; ref: thin space: ' '
+(doom-modeline-def-segment sep-edge
+  "Text style with whitespace."
+
+  (concat
+    ;; (doom-modeline-spc)
+    (propertize " " 'face (if (doom-modeline--active)
+                            'ns/mode-line-sep-edge
+                            'mode-line-inactive))
+    ;; (doom-modeline-spc)
     ))
 
 (setq-ns doom-modeline
@@ -61,124 +174,47 @@
   lsp t
   gnus-timer nil
   irc t ;; circe notifications? no idea if this works
+  before-update-env-hook nil
+  after-update-env-hook nil
   )
-
-;; (setq doom-modeline-buffer-state-icon nil)
-;; (setq doom-modeline-buffer-state-icon nil)
 
 (set-face-attribute 'mode-line nil :height 100)
 (set-face-attribute 'mode-line-inactive nil :height 100)
 
-;; Hooks that run before/after the modeline version string is updated
-(setq doom-modeline-before-update-env-hook nil)
-(setq doom-modeline-after-update-env-hook nil)
-
-;; note to self: abandon this, look at switch-to-next, prev-buffer source, grab that and replace return
-(defun ns/next-buffer-name ()
-  (-first
-    (lambda (bufname) (not (ns/should-skip bufname)))
-    (-map 'buffer-name (window-next-buffers)))
-  )
-
-(defun ns/prev-buffer-name ()
-  (-first
-    (lambda (bufname) (not (ns/should-skip bufname)))
-    (-map 'buffer-name (window-prev-buffers))))
-
-
-(doom-modeline-def-segment next-buffers
-  (when (doom-modeline--active)
-    (propertize
-      (concat
-        ;; todo: mabye [ <buf:-> ¦ <buf:-> ]
-        (doom-modeline-spc)
-        (s-left 8 (ns/prev-buffer-name))
-        " - "
-        (s-left 8 (ns/next-buffer-name))
-        (doom-modeline-spc)
-        )
-      'face 'ns/mode-line-segment
-      )))
-
-(column-number-mode) ;; give us column info in the modeline
-;; todo: get all the faces from these segments/make sure we change their face to be the same as ns/mode-line-segment
-
-(mapcar
-  (fn (set-face-attribute <> nil :background
-        (face-attribute 'ns/mode-line-segment :background)
-        ))
-  '(
-     doom-modeline-buffer-modified
-     doom-modeline-buffer-file
-     doom-modeline-info
-     doom-modeline-debug
-     )
-  )
-
-(doom-modeline-def-segment edge-bar
-  "The bar regulates the height of the mode-line in GUI."
-  (if (doom-modeline--active)
-    doom-modeline--bar-active-edge
-    doom-modeline--bar-inactive))
+(column-number-mode) ; give us column info in the modeline
 
 (doom-modeline-def-modeline 'neeasade-doomline
-  '(matches
-     bar
+  ;; (ns/meta-doom-modeline-def-modeline 'neeasade-doomline
+  '(
+     ;; sep
      remote-host
      buffer-info-neeasade
+     sep
+
      checker
+
+     sep-edge
      ;; bar
-     edge-bar
      selection-info
+     matches
      )
   '(
-     edge-bar
+     ;; bar
+     sep-edge
      next-buffers
-     vcs
+     ;; vcs ;; somehow this one makes the spacing off
      misc-info
-     minor-modes
-     input-method
+     ;; input-method
      ;; buffer-encoding
      ;; major-mode
      ;; process
-     bar
+     ;; sep
+     ;; sep-edge
+     sep
      buffer-position
-     battery
-     bar
+     ;; battery
+     ;; " "
      )
-  )
-
-(defface doom-modeline-bar-edge nil "edge bar" :group 'doom-modeline-faces)
-
-(set-face-attribute 'doom-modeline-bar-edge
-  nil
-  :background
-  ;; (ns/color-lessen 10 (face-attribute 'mode-line :background))
-  "#000000"
-  )
-
-(defun doom-modeline-refresh-bars (&optional width height)
-  "Refresh mode-line bars with `WIDTH' and `HEIGHT'."
-  (let ((width (or width doom-modeline-bar-width))
-         (height (max (or height doom-modeline-height)
-                   (doom-modeline--font-height))))
-    (setq doom-modeline--bar-active
-      (doom-modeline--make-xpm 'doom-modeline-bar width height)
-      doom-modeline--bar-inactive
-      (doom-modeline--make-xpm 'doom-modeline-bar-inactive width height)
-      doom-modeline--bar-active-edge
-      (doom-modeline--make-xpm 'doom-modeline-bar-edge width height)
-      )))
-
-(doom-modeline-def-segment bar
-  "The bar regulates the height of the mode-line in GUI."
-  (if (doom-modeline--active)
-    doom-modeline--bar-active
-    doom-modeline--bar-inactive))
-
-(set-face-attribute 'doom-modeline-bar nil :background
-  (ns/color-greaten 20 (face-attribute 'mode-line :background))
-  ;; "#b9b9b9"
   )
 
 (defun setup-custom-doom-modeline ()
@@ -186,10 +222,12 @@
 
 (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
 
-;; todo: make this unset modeline on not matching spawn
+;; todo: maybe filter out shell-mode and circe buffers here
 (defun! ns/refresh-all-modeline (toggle)
   (ns/setq-local-all
     'mode-line-format
+    (doom-modeline-format--neeasade-doomline)
+
     (if toggle
       ''("%e" (:eval (doom-modeline-format--neeasade-doomline)))
       ;; if we don't want modeline, we still might want
@@ -208,8 +246,6 @@
   (ns/apply-frames (fn nil))
   )
 
-(ns/refresh-all-modeline t)
-
 ;; refesh all mode lines based on the value of the modeline in the current buffer
 (ns/bind "M" (fn! (ns/refresh-all-modeline (not mode-line-format))))
 
@@ -221,20 +257,9 @@
       (setq ns/modeline mode-line-format)
       (setq mode-line-format nil))
     (setq mode-line-format '("%e" (:eval (doom-modeline-format--neeasade-doomline)))))
+
   (redraw-frame))
-
-
-;; (list-faces-display)
-
-;; (set-face-attribute 'mode-line nil :background "#ffffff")
-
-
-;; (set-face-attribute 'mode-line nil :background nil)
-;; (face-attribute 'mode-line :background)
-
-;; (set-face-attribute 'doom-modeline-bar nil :background "#ffffff")
 
 (ns/bind "tm" 'ns/toggle-modeline)
 
-;; force redraw of all frames
-(ns/apply-frames (fn nil))
+(ns/refresh-all-modeline t)
