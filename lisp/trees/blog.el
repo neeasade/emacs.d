5de1670f03a488d94108f0fb195db6c38b9e9f85
neeasade <neeasade@gmail.com>
2020-04-05 08:24:16 -0400

    id's on headlines

--- lisp/trees/blog.el
+++ lisp/trees/blog.el
@@ -184,3 +184,27 @@
 
     (mapcar 'ns/blog-publish-file org-metas)
     ))
+
+
+;; cf https://writequit.org/articles/emacs-org-mode-generate-ids.html#the-problem
+(defun! eos/org-custom-id-get (&optional pom create prefix)
+  "Get the CUSTOM_ID property of the entry at point-or-marker POM.
+   If POM is nil, refer to the entry at point. If the entry does
+   not have an CUSTOM_ID, the function returns nil. However, when
+   CREATE is non nil, create a CUSTOM_ID if none is present
+   already. PREFIX will be passed through to `org-id-new'. In any
+   case, the CUSTOM_ID of the entry is returned."
+  (org-with-point-at pom
+    (let ((id (org-entry-get nil "CUSTOM_ID")))
+      (cond
+        ((and id (stringp id) (string-match "\\S-" id)) id)
+        (create
+          (setq id (s-replace ":" "-" (org-id-new (concat prefix "h"))))
+          (org-entry-put pom "CUSTOM_ID" id)
+          (org-id-add-location id (buffer-file-name (buffer-base-buffer)))
+          id)))))
+
+(defun! eos/org-add-ids-to-headlines-in-file ()
+  "Add CUSTOM_ID properties to all headlines in the
+   current file which do not already have one."
+  (org-map-entries (lambda () (eos/org-custom-id-get (point) 'create))))
