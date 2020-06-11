0484c037591ba97239aedf970c4e78d083a18200
neeasade <neeasade@gmail.com>
2020-06-11 00:22:07 -0500

    iterate

--- lisp/themes/neea-theme.el
+++ lisp/themes/neea-theme.el
@@ -33,41 +33,33 @@
       (+ 0.05 (min rl1 rl2)))))
 
 (defun ns/iterate-color (start op condition)
-  "do OP on START color until CONDITION is met or op has no effect"
+  "Do OP on START color until CONDITION is met or op has no effect."
   (let ((color start))
     (while (and (not (funcall condition color))
              (not (string= (funcall op color) color)))
       (setq color (funcall op color))) color))
 
 (defun ns/color-name-to-lab (name)
-  "transform NAME into LAB colorspace with some lighting assumption"
+  "Transform NAME into LAB colorspace with some lighting assumption."
   (-as-> name <>
     (color-name-to-rgb <>)
     (apply 'color-srgb-to-xyz <>)
-
-    (append <> (list color-d65-xyz))
-    ;; color-d50-xyz | Horizon Light. ICC profile PCS
-    ;; color-d55-xyz | Mid-morning / Mid-afternoon Daylight
-    ;; color-d65-xyz | Noon Daylight: Television, sRGB color space
-    ;; color-d75-xyz | North sky Daylight
-
+    (append <> (list (ht-get ns/theme :white-point)))
     (apply 'color-xyz-to-lab <>)))
 
-;; (ns/color-name-to-lab "#123456")
-
 (defun ns/color-lab-to-name (lab)
-  (->> lab
-    (apply 'color-lab-to-srgb)
+  (->> (append lab (list (ht-get ns/theme :white-point)))
+    (apply 'color-lab-to-xyz)
+    (apply 'color-xyz-to-srgb)
     (apply 'color-rgb-to-hex)))
 
 ;; tests
-;; (->> "#ccddee" color-name-to-rgb (apply 'color-srgb-to-lab)
-;;   (apply 'color-lab-to-srgb)
-;;   (apply 'color-rgb-to-hex)
-;;   ;; (ns/color-lab-to-name)
-;;   )
+(->> "#ccddee" color-name-to-rgb (apply 'color-srgb-to-lab)
+  ;; (apply 'color-lab-to-srgb)
+  ;; (apply 'color-rgb-to-hex)
+  (ns/color-lab-to-name)
+  )
 
-;; (color-distance)
 
 (defun ns/color-name-distance (c1 c2)
   ;; note: there are 3 additional optional params to cie-de2000: compensation for {lightness,chroma,hue}
@@ -76,25 +68,54 @@
     (ns/color-name-to-lab c1)
     (ns/color-name-to-lab c2)))
 
-(ns/color-name-distance "#000000" "#ffffff")
+;; (ns/color-name-distance "#000000" "#ffffff")
+;; the builtin:
+;; (color-distance "#000000" "#ffffff")
 
-(ns/iterate-color
-  "#000000"
-  (lambda (c) (color-lighten-name c 1))
-  (lambda (color)
-    (>
-      (ns/color-name-distance "#000000" color)
-      ;; (ns/color-contrast "#000000" color)
-      10
-      ;; 6.8
-      )
-    ))
+;; (ns/iterate-color "#ffffff"
+;;   (lambda (c) (color-darken-name c 0.5))
+;;   (lambda (color)
+;;     (>
+;;       ;; (ns/color-name-distance "#ffffff" color)
+;;       (ns/color-contrast "#ffffff" color)
+;;       19
+;;       ;; 6.8
+;;       )))
 
-;; (defun ns/theme (key)
-;;   (pcase key
-;;     ('foreground (ns/longen-color "#444444"))
-;;     ('background (ns/longen-color "#eeeeee"))
-;;     ('star (ns/convert-color "#9999cc"))))
+;; some thoughts:
+;; contrast ratio as measurement is really only good as luminance meatric
+;; color distance is useful if we're doing weird things maybe?
+
+(setq ns/theme
+  (let*
+    ((foreground 'todo)
+      (background 'todo)
+      (accent1 'todo)
+      (accent2 'todo))
+
+    (ht
+      (:foreground foreground)
+      (:background background)
+
+      (:faded 'todo)
+
+      (:star background)
+      (:star-1 background)
+      (:star-2 background)
+
+      (:moon background)
+      (:moon-1 background)
+      (:moon-2 background)
+
+      (:white-point
+        color-d65-xyz
+        ;; color-d50-xyz | Horizon Light. ICC profile PCS
+        ;; color-d55-xyz | Mid-morning / Mid-afternoon Daylight
+        ;; color-d65-xyz | Noon Daylight: Television, sRGB color space
+        ;; color-d75-xyz | North sky Daylight
+        ))))
+
+(ht-get ns/theme :white-point)
 
 ;; (apply 'color-rgb-to-hsl (color-name-to-rgb "#ccddee"))
 
@@ -132,32 +153,45 @@
 (base16-theme-define 'neea
   (list
     ;; The comments on the sections here are from the base16 styling guidelines, not necessarily
-    ;; what the emacs package follows.
+    ;; what the emacs base16 theme package follows.
 
-    ;; note: fade values might need to change based on light/dark emthasis color, fg bg
+    ;; guidelines location: http://chriskempson.com/projects/base16/
+    ;; I've also noted some faces I care about
+
     :base00 background ;; Default Background
 
-    ;; you'll want to match these
+    ;; ivy-current-match background
     :base01 (ns/fade-min-contrast star 100 2) ;; Lighter Background (Used for status bars)
+
     :base02 (ns/fade-min-contrast star 100 2) ;; Selection Background
 
     :base03 (ns/fade-color foreground 20) ;; Comments, Invisibles, Line Highlighting
     :base04 foreground                    ;; Dark Foreground (Used for status bars)
+
     :base05 foreground                    ;; Default Foreground, Caret, Delimiters, Operators
+
     :base06 foreground                    ;; Light Foreground (Not often used)
-    :base07 background                    ;; Light Background (Not often used)
+
+    :base07 background ;; Light Background (Not often used)
+
+    ;; org-todo
     :base08 (ns/fade-color star 15) ;; Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+
+    ;; ivy-current-match foreground
     :base09 foreground              ;; Integers, Boolean, Constants, XML Attributes, Markup Link Url
 
     :base0A (ns/fade-color star 20) ;; Classes, Markup Bold, Search Text Background
 
     :base0B star       ;; Strings, Inherited Class, Markup Code, Diff Inserted
+
     :base0C foreground ;; Support, Regular Expressions, Escape Characters, Markup Quotes
 
-    ;; prompt
+    ;; prompt, function-name
     :base0D (ns/bolden-color star 30) ;; Functions, Methods, Attribute IDs, Headings
 
+    ;; keyword-face, org-date
     :base0E star       ;; Keywords, Storage, Selector, Markup Italic, Diff Changed
+
     :base0F foreground ;; Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
     ))
 
