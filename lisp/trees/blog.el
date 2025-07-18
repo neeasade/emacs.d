913e2ae0358af72138feeec20558cca643b6aebc
neeasade <neeasade@users.noreply.github.com>
2025-07-18 13:48:52 -0500

    blog render updates
    move some conditionals up into the template, use mustache.el, remove double
    render

--- lisp/trees/blog.el
+++ lisp/trees/blog.el
@@ -1,12 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 
-
 (setq ns/blog-title "ＳＴＸ")
 (setq ns/blog-cache (-ht))
-
-(ns/comment
-  (measure-time
-    (ns/blog-generate-all-files)))
 
 ;;* compat
 (defalias 'tarp/get 'myron-get)
@@ -133,13 +128,21 @@
            (-partition-in-steps 3 1 posts))
        ,(-last-item posts) (,(first (-take-last 2 posts)) nil))))
 
+(ns/use mustache)
+(require 'xml)
+
 (defun ns/mustache (text table)
-  "Basic mustache templating."
-  (llet [replace-map (ht-amap `(,(format "{{%s}}" (ns/str key)) . ,(or value "")) table)]
-    ;; 2x for inline templates
-    (->> text
-      (s-replace-all replace-map)
-      (s-replace-all replace-map))))
+  (llet [mustache-key-type 'keyword]
+    ;; debug, print nil keys (can be used as bools for mustache but not templated)
+    (comment
+      (ht-map
+        (lambda (k v)
+          (when (and (not v) (not (s-starts-with? "is-" (ns/str k))))
+            (message (ns/str "nil key: " k))))
+        table))
+    (-> text
+      (mustache-render (ht->alist table))
+      (xml-substitute-special))))
 
 ;;* render
 ;; for fontifying src blocks
@@ -162,12 +165,46 @@
     (apply '-concat)
     (apply '-ht)))
 
+(defun ns/path-to-slug (path)
+  "File path to html slug (basename)"
+  (->> (--reduce-from (s-replace it "" acc)
+         (f-base path)
+         (s-split "" ";/?:@&=+$,'"))
+    (s-replace-regexp (pcre-to-elisp "[0-9]-[0-9]{2}-[0-9]{2}-") "")
+    (s-downcase)))
+
+(defun ns/blog-file-to-meta (path)
+  "File path to metadata ."
+  ;; (message (format "BLOG: generating meta for %s" path))
+  (llet [org-file-content (f-read path)
+          props (ns/blog-get-properties org-file-content)]
+    (-ht :path path
+      :content (s-replace-regexp "^-----$" (ns/blog-make-hsep) org-file-content)
+      :draft-p (ht-get props "draft")
+      :title (ht-get props "title" "(untitled)")
+      :rss-title (ht-get props "rss_title")
+      :subtitle (ht-get props "title_extra" "")
+      :type (llet [parent-dir (->> path f-parent f-base)]
+              (substring parent-dir 0 (1- (length parent-dir))))
+      :published-date  (first
+                         (--keep (first (s-match (pcre-to-elisp "[0-9]{4}-[0-9]{2}-[0-9]{2}") it))
+                           (list (ht-get props "pubdate" "")
+                             (f-base path))))
+      :edited-date (let ((git-query-result (sh (format "cd '%s'; git log --follow -1 --format=%%cI '%s'"
+                                                 ;; appease the shell.
+                                                 (s-replace "'" "'\\''" (f-dirname path))
+                                                 (s-replace "'" "'\\''" path)))))
+                     (if (s-blank-p git-query-result) ""
+                       (substring git-query-result 0 10)))
+      :url (format "https://notes.neeasade.net/%s.html" (ns/path-to-slug path))
+      :html-dest (format "%s/%s.html" (ns/blog-path "published") (ns/path-to-slug path))
+      :csslinks (ns/blog-get-csslinks))))
+
 (defun ns/blog-render-org (org-meta-table)
   (ns/mustache (f-read (~e "org/blog_template.org"))
     (ht-merge org-meta-table
       (llet ((&hash :type :path :subtitle :published-date :edited-date) org-meta-table)
         (-ht
-          :blog-title ns/blog-title
           :up (llet [(dest label)
                       (cond
                         ((s-starts-with-p "index" (f-filename path)) '("https://neeasade.net" "splash"))
@@ -175,72 +212,19 @@
                           '("/sitemap.html" "sitemap"))
                         (t `("/index.html" ,ns/blog-title)))]
                 (format "<a href='%s'>../%s</a>" dest label))
-
           :og-description (->> (or subtitle "")
                             (s-replace-regexp "{{{.*(" "")
                             (s-replace ")}}}" ""))
-
           :page-markup-link (format "https://raw.githubusercontent.com/neeasade/neeasade.github.io/source/%ss/%s"
                               type (f-filename path))
-
           :page-history-link (format "https://github.com/neeasade/neeasade.github.io/commits/source/%ss/%s"
                                type (f-filename path))
-
-          :sitemap-link (when (s-starts-with-p "index" (f-filename path))
-                          "<a href='/sitemap.html'>sitemap</a>")
-          :pubinfo (when (string= type "post")
-                     (s-join "\n"
-                       `("<div class=pubinfo>"
-                          ,(if (string= published-date edited-date)
-                             "Published {{published-date}}"
-                             "Published {{published-date}}, last edit <a href=\"{{page-history-link}}\">{{edited-date}}</a>")
-                          "</div>")))
-          :footer-center (when (s-starts-with-p "index" (f-filename path))
-                           "<a href='https://webring.xxiivv.com/#random' target='_blank'><img style='width:40px;height:40px' src='./assets/img/logos/xxiivv.svg'/></a>
-  <a href='https://github.com/nixers-projects/sites/wiki/List-of-nixers.net-user-sites' target='_blank'><img style='width:35px;height:40px' src='./assets/img/logos/nixers.png'/></a>
-  <a href='https://webring.recurse.com'><img alt='Recurse Center Logo' src='./assets/img/logos/recurse.png' style='height:40px;width:40px;'></a> ")
-
-          :flair (when (or (string= type "post")
-                         (string= type "note"))
-                   "@@html:<div class='flair'></div>@@"))))))
-
-(defun ns/path-to-slug (path)
-  "File path to html slug (basename)"
-  (->> (--reduce-from (s-replace it "" acc)
-         (f-base path)
-         (s-split "" ";/?:@&=+$,'"))
-    (s-replace-regexp (pcre-to-elisp "[0-9]{4}-[0-9]{2}-[0-9]{2}-") "")
-    ;; (s-downcase)
-    ))
-
-(defun ns/blog-file-to-meta (path)
-  "File path to metadata ."
-  ;; (message (format "BLOG: generating meta for %s" path))
-  (llet [org-file-content (f-read path)
-          props (ns/blog-get-properties org-file-content)]
-    (-ht
-      :path path
-      :content (s-replace-regexp "^-----$" (ns/blog-make-hsep) org-file-content)
-      :draft-p (ht-get props "draft")
-      :title (ht-get props "title" "(untitled)")
-
-      :rss-title (ht-get props "rss_title")
-      :subtitle (ht-get props "title_extra")
-      :type (llet [parent-dir (->> path f-parent f-base)]
-              (substring parent-dir 0 (1- (length parent-dir))))
-      :published-date (first
-                        (--keep (first (s-match (pcre-to-elisp "[0-9]{4}-[0-9]{2}-[0-9]{2}") it))
-                          (list (ht-get props "pubdate" "")
-                            (f-base path))))
-      :edited-date (let ((git-query-result (sh (format "cd '%s'; git log --follow -1 --format=%%cI '%s'"
-                                                 ;; appease the shell.
-                                                 (s-replace "'" "'\\''" (f-dirname path))
-                                                 (s-replace "'" "'\\''" path)))))
-                     (when-not (s-blank-p git-query-result)
-                       (substring git-query-result 0 10)))
-      :url (format "https://notes.neeasade.net/%s.html" (ns/path-to-slug path))
-      :html-dest (format "%s/%s.html" (ns/blog-path "published") (ns/path-to-slug path))
-      :csslinks (ns/blog-get-csslinks))))
+          :blog-title ns/blog-title
+          :is-index (s-starts-with-p "index" (f-filename path))
+          :is-page (string= type "page")
+          :is-post (string= type "post")
+          :is-note (string= type "note")
+          :is-edited (and (not (s-blank? edited-date)) (not (string= published-date edited-date))))))))
 
 (defun! ns/blog-publish-meta (org-meta)
   (let ((default-directory (ns/blog-path "published"))
@@ -321,7 +305,6 @@
     (fn (-map 'ns/blog-file-to-meta
           (ht-keys (ns/get-blog-files))))))
 
-
 (defun ns/blog-generate (metas)
   (setq
     ns/theme (ht-get myron-themes-colors :normal) ; compat
@@ -378,6 +361,7 @@
               (s-join "\n")))))
 
 (ns/bind
+  "ob" 'ns/blog-generate-and-open-current-file
   "iq" (fn!! insert-blog-link
          (llet [posts (ns/get-blog-files)
                  title (ns/pick (ht-values posts))
